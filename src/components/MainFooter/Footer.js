8b9a4f07a0cdf0af4bfb3d490e04f4d50af94ef1
Krishna <krishnakishore.s219@gmail.com>
2021-07-28 15:59:27 +0530

    Changes done to footer

--- src/components/MainFooter/Footer.js
+++ src/components/MainFooter/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
                 <div className="me-5" >
 
                     <a href="" className="text-dark me-4">
-                        Home
+                        Hom
                     </a>
                     <a href="" className="text-dark me-4">
                         About Us
